166c771cd25b29ba23ea407877e447b7397d6f84
Servidor-Grafo <stevealv@hotmail.com>
2020-02-20 23:57:10 -0600

    Initial commit

---
+++ Socket.hpp
@@ -0,0 +1,14 @@
+//
+// Created by steve on 20/02/20.
+//
+
+#ifndef CLIENT_GRAFO_SOCKET_HPP
+#define CLIENT_GRAFO_SOCKET_HPP
+
+
+class Socket {
+
+};
+
+
+#endif //CLIENT_GRAFO_SOCKET_HPP
---
+++ cmake-build-debug/CMakeFiles/Client_Grafo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client_Grafo.dir/main.cpp.o"
+  "CMakeFiles/Client_Grafo.dir/Socket.cpp.o"
+  "Client_Grafo.pdb"
+  "Client_Grafo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Client_Grafo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
